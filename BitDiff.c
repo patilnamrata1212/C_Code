986ad5cdcde7556a393e4b13034e320ff47bb182
patilnamrata1212 <75855466+patilnamrata1212@users.noreply.github.com>
2021-04-03 17:32:46 +0530

    Find number of bits flip to convert from a to b
    You are given two numbers A and B. The task is to count the number of bits 
    needed to be flipped to convert A to B.
    
    Example 1:
    
    Input: A = 10, B = 20
    Output: 4
    Explanation:
    A  = 01010
    B  = 10100
    As we can see, the bits of A that need 
    to be flipped are 01010. If we flip 
    these bits, we get 10100, which is B.

---
+++ BitDiff.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+You are given two numbers A and B. The task is to count the number of bits 
+needed to be flipped to convert A to B.
+
+Example 1:
+
+Input: A = 10, B = 20
+Output: 4
+Explanation:
+A  = 01010
+B  = 10100
+As we can see, the bits of A that need 
+to be flipped are 01010. If we flip 
+these bits, we get 10100, which is B.
+
+*******************************************************************************/
+#include <stdio.h>
+int BitDiff(int a,int b)
+{
+    int temp_a=0,temp_b=0;
+    int count = 0;
+    while(a != 0 & b != 0)
+    {
+        temp_a = a >> 1;
+        temp_b = b >> 1;
+        if (temp_b != temp_a )
+        {
+            ++count;
+        }
+        a = a >> 1;
+        b = b >> 1;
+    }
+    return count;
+}
+int main()
+{
+    int a =25,b=20;
+   printf("%d\n",BitDiff(a,b));
+   return 0;
+
+}
