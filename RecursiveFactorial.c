8e14b363c732721e55008471b575faaa837a6af2
patilnamrata1212 <75855466+patilnamrata1212@users.noreply.github.com>
2022-06-28 00:07:36 +0530

    optimize solution added below

--- RecursiveFactorial.c
+++ RecursiveFactorial.c
@@ -16,3 +16,22 @@ int main ()
   factorial(n);
   return 0;
 }
+
+
+// Another  approch 
+#include <stdio.h>
+int factorial(int n)
+{
+    if (n <= 1)
+    {
+      return 1;
+    }
+    return n*factorial(n-1);
+}
+ 
+int main ()
+{
+  int n = 5;
+  printf("%d",factorial(n));
+  return 0;
+}
